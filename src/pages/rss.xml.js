c89065242263ed4e35ccf2637336bc0e5031a699
saltyaom <saltyaom@gmail.com>
2024-12-21 05:46:57 +0700

    :blue_book: doc: use pekomama cover

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,16 +2,16 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
 export async function GET(context) {
-    const posts = await getCollection('blog')
+	const posts = await getCollection('blog')
 
 	return rss({
-        title: 'SaltyAom',
-        description: 'SaltyAom, open source maintainer and cosplayer',
-        site: context.site,
-        items: posts.map((post) => ({
-            ...post.data,
-			pubDate: new Date().toString(),
-            link: `/blog/${post.slug}`
-        }))
-    })
+		title: 'SaltyAom',
+		description: 'SaltyAom, open source maintainer and cosplayer',
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			pubDate: post.data.date.toString(),
+			link: `/blog/${post.slug}`
+		}))
+	})
 }
