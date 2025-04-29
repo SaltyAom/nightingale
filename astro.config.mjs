c7c190afd24307e496ba5d2f462d1afb4f4a1df7
saltyaom <saltyaom@gmail.com>
2025-04-29 07:10:56 +0700

    :tada: feat: layout stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,22 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 import syntax from './config/syntax-theme.json'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://saltyaom.com',
-	integrations: [mdx(), sitemap(), react(), tailwind()],
+	integrations: [mdx(), sitemap(), react()],
+
 	markdown: {
 		shikiConfig: {
 			theme: syntax
 		}
+	},
+
+	vite: {
+		plugins: [tailwindcss()]
 	}
 })
